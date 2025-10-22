6f9cec71bab63790a1db1796ff92c03154c3df3a
andreluiz05 <andreluizdesantanasilva123@gmail.com>
2025-10-22 18:37:55 -0300

    questao 5.py

--- questao 5.py
+++ questao 5.py
@@ -1,11 +1,14 @@
-entrada_user = str(input("Digite seu nome de usuário! "))
-entrada_pass = str(input("Digite sua senha, mas que seja diferente do nome usuário! "))
+entrada_user = input("Digite seu nome de usuário: ")
+entrada_pass = input("Digite sua senha, mas que seja diferente do nome de usuário: ")
 
 var1 = entrada_user
 var2 = entrada_pass
 
 if var1 == var2:
-    print("Usuário e senha iguais! Digite com informações!")
-    
+    print("Usuário e senha iguais! Digite com com informações!")
+   
+ while var2 == var1:
+        var2 = input("Digite uma senha diferente do nome usuário: ")
+
 elif var1 != var2:
     print("Usuário e senha aceitos! Cadastro concluído!")
